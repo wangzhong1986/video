5c534ab761d68efd67a7966feec8f00842155939
王众 <wangzhong@wangzhongdeimac.local>
2016-07-13 09:35:23 +0800

    Initial Commit

---
+++ WZPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WZPlayer
+//
+//  Created by wangzhong on 16/7/13.
+//  Copyright © 2016年 wangzhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
